0596bb316fbd34f2843c4398670de5883a7aef85
vtt-cloud <57213632+vtt-cloud@users.noreply.github.com>
2024-09-16 05:15:17 +0700

    Update sum2num.js
    change the body of sum2Num

--- sum2num.js
+++ sum2num.js
@@ -1,9 +1,9 @@
 function sum2Num(n1, n2, n3) {
     n = parseInt(n1.value) + parseInt(n2.value);
     if (n == parseInt(n3.value)) { 
-      alert('Good job! ' + n3.value);
+      alert('Good job!');
     } 
     else {
-      alert('Bad job! ' + n);
+      alert('Bad job! Result must be' + n);
     }   
   }
