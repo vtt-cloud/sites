42562cf3325942a28845b4e9a0adf2023178d152
vtt-cloud <vuthitra@gmail.com>
2024-09-17 12:42:23 +0700

    release version 2.0 by VTT on Sep 17, 2024

--- sum2num.js
+++ sum2num.js
@@ -1,9 +1,15 @@
-function sum2Num(n1, n2, n3) {
-    n = parseInt(n1.value) + parseInt(n2.value);
-    if (n == parseInt(n3.value)) { 
-      alert('Good job!');
-    } 
-    else {
-      alert('Bad job! Result must be ' + n);
-    }   
+function sum2Num(){  
+  let n1 = parseInt(document.getElementById("num1").value);
+  let n2 = parseInt(document.getElementById("num2").value);
+  let n3 = parseInt(document.getElementById("num3").value);
+  let n = n1 + n2;
+  if (n == n3) { 
+    alert('Good job!');
+    //result = "Good job!";
+  } 
+  else {
+    alert('Bad job! Result must be ' + n);
+    //result = "Bad job! Result must be " + n;
   }
+  //document.getElementById("result").innerHTML = result;
+}
